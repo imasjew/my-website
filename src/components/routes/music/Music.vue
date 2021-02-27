ddd95a4a8728934ab25fc98304d5cf86845f4298
imasjew <yif@http://192.168.1.10:10082/RD/frontend-dev-website.git.com>
2021-02-27 18:45:45 +0800

    创建音乐组件底部播放栏

--- src/components/routes/music/Music.vue
+++ src/components/routes/music/Music.vue
@@ -3,9 +3,10 @@
     music
     <button @click="goToPage('musiclist')">list</button>
     <button @click="goToPage('song')">song</button>
-    <router-view/>
+    <button @click="openDrawer()">drawer</button>
+    <div class="song-controller">footer</div>
+    <router-view />
   </div>
-
 </template>
 
 <script>
@@ -14,15 +15,27 @@ export default {
   data() {
     return {
       activeIndex: "",
+      drawer: false,
     };
   },
   methods: {
     goToPage(key) {
-      this.$router.push('/home/music/' + key);
-    }
-  }
+      this.$router.push("/home/music/" + key);
+    },
+    openDrawer: function () {
+      this.drawer = true;
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="less">
+.song-controller{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 30px;
+  border: 5px solid black;
+  box-sizing: border-box;
+}
 </style>
