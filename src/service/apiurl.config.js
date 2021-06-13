09c00c00313a17f53d110b7a933766f6301854b6
imasjew <yif@http://192.168.1.10:10082/RD/frontend-dev-website.git.com>
2021-06-14 03:42:06 +0800

    原本的服务器挂了，起个本地node服务

--- src/service/apiurl.config.js
+++ src/service/apiurl.config.js
@@ -1,9 +1,10 @@
-const MUSICURL = 'http://www.hjmin.com/';
+const LOCALMUSICURL = 'http://localhost:3000/';
+// 已失效 const MUSICURL = 'http://www.hjmin.com/';
 
 export const APIURL = {
-  song_list: MUSICURL + 'search?keywords=',
-  song_url: MUSICURL + 'song/url?id=',
-  check_song: MUSICURL + 'check/music?id=',
-  get_song_info: MUSICURL + 'song/detail?ids=',
-  get_lyric: MUSICURL + 'lyric?id='
+  song_list: LOCALMUSICURL + 'search?keywords=',
+  song_url: LOCALMUSICURL + 'song/url?id=',
+  check_song: LOCALMUSICURL + 'check/music?id=',
+  get_song_info: LOCALMUSICURL + 'song/detail?ids=',
+  get_lyric: LOCALMUSICURL + 'lyric?id='
 }
